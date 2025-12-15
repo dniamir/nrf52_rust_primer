75a215d0c72ab0e780109d086bf22d1807bc7a42
Darien Niamir <darien.niamir@gmail.com>
2025-12-15 00:10:49 -0800

    Ensure build.rs is run anytime memory flags are changed

--- build.rs
+++ build.rs
@@ -5,6 +5,13 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
 
+    // Force rebuild when feature set changes
+    println!("cargo:rerun-if-env-changed=CARGO_FEATURE_BLE_MEMORY");
+    println!("cargo:rerun-if-env-changed=CARGO_FEATURE_DEFAULT_MEMORY");
+
+    // Always regenerate memory.x
+    let _ = fs::remove_file("memory.x");
+
     // Decide which memory layout to use
     let memory_file = if cfg!(feature = "ble_memory") {
         "INCLUDE memory/memory_ble.x\n"
@@ -14,8 +21,8 @@ fn main() {
         panic!("One of 'default_memory' or 'ble_memory' must be enabled");
     };
 
-    // 🔑 This creates memory.x for link.x to include
-    fs::write("memory.x", memory_file).unwrap();
+    // This creates memory.x for link.x to include
+    fs::write("memory.x", memory_file).expect("Failed to write memory.x");
 
     // Ensure rebuilds when memory layouts change
     println!("cargo:rerun-if-changed=memory/memory_default.x");
