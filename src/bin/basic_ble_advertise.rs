ba49bec57b1c3a22d12204d68451168931a02016
Darien Niamir <darien.niamir@gmail.com>
2025-12-15 02:20:31 -0800

    Print the BLE address and write comments on the BLE script

--- src/bin/basic_ble_advertise.rs
+++ src/bin/basic_ble_advertise.rs
@@ -11,6 +11,7 @@ use defmt::{info, *};
 use embassy_executor::Spawner;
 use nrf_softdevice::ble::advertisement_builder::{Flag, LegacyAdvertisementBuilder, LegacyAdvertisementPayload, ServiceList, ServiceUuid16,};
 use nrf_softdevice::ble::peripheral;
+use nrf_softdevice::ble;
 use nrf_softdevice::{raw, Softdevice};
 
 #[embassy_executor::task]
@@ -21,9 +22,6 @@ async fn softdevice_task(sd: &'static Softdevice) -> ! {
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!("Hello World!");
-
-    // let addr = nrf_softdevice::ble::address_get(sd);
-    // info!("BLE addr: {:02x?}", addr.bytes());
 
     let config = nrf_softdevice::Config {
         clock: Some(raw::nrf_clock_lf_cfg_t {
@@ -57,25 +55,34 @@ async fn main(spawner: Spawner) {
         ..Default::default()
     };
 
-    let sd = Softdevice::enable(&config);
-    unwrap!(spawner.spawn(softdevice_task(sd)));
+    let sd = Softdevice::enable(&config);  // Enable Nordic BLE SoftDevice with specific config (more like on device settings)
+    unwrap!(spawner.spawn(softdevice_task(sd)));            // Starts the soft device
 
-    let mut config = peripheral::Config::default();
-    config.interval = 50;
+    // Print SoftDevice BLE address - should already be a unique address from the FICR (Factory Information Configuration Registers)
+    let [a, b, c, d, e, f] = ble::get_address(sd).bytes();
+    defmt::info!("BLE addr: {:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}", f, e, d, c, b, a);
 
-    static ADV_DATA: LegacyAdvertisementPayload = LegacyAdvertisementBuilder::new()
-        .flags(&[Flag::GeneralDiscovery, Flag::LE_Only])
-        .services_16(ServiceList::Complete, &[ServiceUuid16::HEALTH_THERMOMETER]) // if there were a lot of these there may not be room for the full name
-        .short_name("Hello")
+
+    let mut config = peripheral::Config::default(); // Creates default config for peripheral (more like advertisement settings) (advertising interval, transmission power, channels, adress type, etc...)
+    config.interval = 50;                                   // 50 BLE units of time (0.625 ms -> 31.25ms) - wakes up radio every 31ms to send packet for a few ms
+
+    static ADV_DATA: LegacyAdvertisementPayload = LegacyAdvertisementBuilder::new()     // Starts building the main BLE advertising payload
+        .flags(&[Flag::GeneralDiscovery, Flag::LE_Only])                                // Marks the device as discoverable and BLE only
+        .services_16(ServiceList::Complete, &[ServiceUuid16::HEALTH_THERMOMETER])       // Advertises the device as a health thermometer
+        .short_name("Hello")                                                            // Short device name to fit into limited advertisement space
         .build();
 
     // but we can put it in the scan data
     // so the full name is visible once connected
-    static SCAN_DATA: LegacyAdvertisementPayload = LegacyAdvertisementBuilder::new().full_name("Hello, Rust!").build();
+    // Defines extra data if requested
+    static SCAN_DATA: LegacyAdvertisementPayload = LegacyAdvertisementBuilder::new().full_name("hello, Rust!").build();
 
+    // Chooses a non-connectable but scanning advertisement mode - adds the settings previously chosen
     let adv = peripheral::NonconnectableAdvertisement::ScannableUndirected {
         adv_data: &ADV_DATA,
         scan_data: &SCAN_DATA,
     };
+
+    // Starts BLE service
     unwrap!(peripheral::advertise(sd, adv, &config).await);
 }
